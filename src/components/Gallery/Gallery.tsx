677bc320fe7445b5e6e19fe9dfe59d677265f5fb
karpss <ola.fam10@gmail.com>
2022-07-23 11:33:10 +0100

    Gallery component now gets data from API

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,9 +1,56 @@
 /* eslint-disable */
+import React, {useEffect, useReducer} from 'react';
+import axios from 'axios';
+import reducer from '../../reducer/Reducer';
+import './Gallery.css';
 
 const Gallery = () => {
-  return (
-    <div>Gallery Component</div>
-  )
+    const [{data, isLoading, error}, dispatch] = useReducer(reducer, {
+        isLoading: false,
+      });
+    
+      useEffect(() => {
+        let ignore = false;
+    
+        dispatch({type: 'request'});
+        axios(
+          `https://www.rijksmuseum.nl/api/nl/collection?key=yW6uq3BV&involvedMaker=Rembrandt+van+Rijn`
+        ).then(
+          (results) => {
+            if (!ignore) dispatch({type: 'success', results: results.data});
+          },
+          (error: string) => dispatch({type: 'failure', error})
+        );
+    
+        return (): void => {
+          ignore = true;
+        };
+      }, []);
+    
+      // console.log(data);
+    
+      return (
+        <>
+          <h1>MUSEUM</h1>
+          <div className="container">
+            {isLoading && <span>Loading...</span>}
+            {error && <span>Error: {error}</span>}
+            <ul>
+              {data &&
+                data.artObjects &&
+                data.artObjects.map((item) => (
+                  <div className="card" key={item.id}>
+                    <img
+                      src={item.webImage.url}
+                      alt="Gallery Images"
+                    />
+                    <p>{item.longTitle}</p>
+                  </div>
+                ))}
+            </ul>
+          </div>
+        </>
+      );
 }
 
-export default Gallery
+export default Gallery;
