9574a2947fa184ec7af4240080363bf8794de60b
karpss <ola.fam10@gmail.com>
2022-07-23 10:27:12 +0100

    Folder structure set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gallery from "./components/Gallery/Gallery";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+const Gallery = () => {
+  return (
+    <div>Gallery Component</div>
+  )
+}
+
+export default Gallery
