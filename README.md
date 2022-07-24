fe8d8caa4021c2774efb40b643faa14bc7d908b0
karpss <ola.fam10@gmail.com>
2022-07-24 09:30:23 +0100

    Updated Readme with some explanation

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Displaying a view of thumbnails from the Het Rijksmuseum
 - Inside this Gallery folder there is a ```Gallery.tsx``` component that makes the request and renders the data and ```Gallery.css``` file that styles the component. Tests for this component can be found in the ```__test__``` folder in this directory.
 - There is also a reducer folder that hosts the exported reducer function in ```Reducer.tsx```. This is used to indicate that the data fetching operation is in progress and if it fails, it shows an error message.
 - ```mockData.ts``` is used to mock queried data for testing.
+- Two third party libraries were used. Axios, a promise based http client used to fetch data from the Api and React Lazy Load Image component. The choice of this library was because it is an easy to use solution.
 
--- src/components/Gallery/__test__/Gallery.test.tsx
+++ src/components/Gallery/__test__/Gallery.test.tsx
@@ -1,28 +1,21 @@
 /* eslint-disable */
-import { render, screen } from "@testing-library/react";
+import React, { render, screen } from "@testing-library/react";
 import { mockData } from "../../../mockData";
 import Gallery from "../Gallery";
 
 test("renders Museum Header", () => {
-    render(<Gallery />);
-    const headerElement = screen.getByText(/MUSEUM/i);
-    expect(headerElement).toBeInTheDocument();
-  });
+  render(<Gallery />);
+  const headerElement = screen.getByText(/MUSEUM/i);
+  expect(headerElement).toBeInTheDocument();
+});
 
+test("shows Loading message while fetching request", () => {
+  render(<Gallery />);
+  const loadingMessage = screen.getByText(/Loading.../i);
+  expect(loadingMessage).toBeInTheDocument();
+});
 
-  test('shows Loading message while fetching request', () => {
-    const {getByText} = render(<Gallery />)
-  
-    expect(getByText(/Loading.../i)).toBeInTheDocument()
-  })
-
-
-  
-    // test("renders gallery details", () => {
-    //   render(<Gallery {...mockData.artObjects} />);
-    //   const title = screen.getByText(/Zelfportret als de apostel Paulus, Rembrandt van Rijn, 1661/i)
-    //   expect(title).toBeInTheDocument();
-    // });
-
-
-  
+test("matches snapshot", () => {
+  const { container } = render(<Gallery {...mockData} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
